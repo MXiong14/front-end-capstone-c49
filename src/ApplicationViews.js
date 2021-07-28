b5d030a07290f3f5105c665c6d08113d8fc3bfcd
Matt <Mattxiong_14@hotmail.com>
2021-07-28 15:34:35 -0500

    added all functionality to all basketball forms. will allow users full use of crud in all modules of both baseball and basketball

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -4,14 +4,20 @@ import { Home } from "./Home"
 import { BaseballForm } from "./Baseball/BaseballForm"
 import { BaseballList } from "./Baseball/BaseballList"
 import { BaseballDetail } from "./Baseball/BaseballDetail"
-
 import { BaseballProvider } from "./Baseball/BaseballProvider"
+import { BasketballForm } from "./Basketball/BasketballForm"
+import { BasketballList } from "./Basketball/BasketballList"
+import { BasketballDetail } from "./Basketball/BasketballDetail"
+import { BasketballProvider } from "./Basketball/BasketballProvider"
+
+
 
 export const ApplicationViews = () => {
     return (
         <>
         
         <BaseballProvider>
+        <BasketballProvider>
             {/* Render the location list when http://localhost:3000/ */}
             <Route exact path="/">
                 <Home />
@@ -24,13 +30,27 @@ export const ApplicationViews = () => {
       <Route exact path="/baseballs/create">
         <BaseballForm />
       </Route>
-      <Route path="/baseballs/edit/:baseballId(\d+)">
+      <Route path="/baseball/edit/:baseballId(\d+)">
         <BaseballForm />
       </Route>
-      <Route exact path="/baseballs/detail/:baseballId(\d+)">
+      <Route exact path="/baseball/detail/:baseballId(\d+)">
         <BaseballDetail />
       </Route>
+
+      <Route exact path="/basketball">
+        <BasketballList />
+      </Route>
+      <Route exact path="/basketball/create">
+        <BasketballForm />
+      </Route>
+      <Route path="/basketball/edit/:basketballId(\d+)">
+        <BasketballForm />
+      </Route>
+      <Route exact path="/basketball/detail/:basketballId(\d+)">
+        <BasketballDetail />
+      </Route>
        
+        </BasketballProvider>
         </BaseballProvider>
         </>
     )
--- src/Basketball/BasketballCard.js
+++ src/Basketball/BasketballCard.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "./Basketball.css"
 //this renders the cards one by one
 export const BasketballCard = ({ basketball }) => (
     <section className="basketball">
         <h3 className="basketball_name">
-          <Link to={ `/basketballs/detail/${basketball.id}` }>
+          <Link to={ `/basketball/detail/${basketball.id}` }>
             { basketball.playerName }
           </Link>
         </h3>
